8acbc538987263c2174b5c0de237b6378b20dc04
doradosystemsro <doradosystems.ro@gmail.com>
2026-05-30 04:31:55 +0300

    Update config.mjs

--- tina/config.mjs
+++ tina/config.mjs
@@ -1,27 +1,26 @@
 import { defineConfig } from "tinacms";
 
 export default defineConfig({
-  branch: "main", 
-  clientId: "8640bb63-3016-4565-a65a-62829d5758f7", // Client ID-ul tău corect
-  token: process.env.TINA_TOKEN, // Va citi token-ul adăugat de tine în Cloudflare
-  
+  branch: "main",
+  clientId: "8640bb63-3016-4565-a65a-62829d5758f7",
+  token: "c14c4e689dd6159dc6c5af7649cc30a6bc592dac", // Token-ul tău direct
   build: {
-    outputFolder: "admin", // Tina va crea interfața în folderul servicepclaptop.pages.dev/admin
-    publicFolder: ".",     // Rădăcina site-ului tău HTML
+    outputFolder: "admin",
+    publicFolder: ".",
   },
   schema: {
     collections: [
       {
         name: "post",
         label: "Articole Blog",
-        path: "blog", // Salvează direct în folderul tău existent numit blog
+        path: "blog",
         format: "md",
         fields: [
           { type: "string", name: "title", label: "Titlu Articol (H1)", isTitle: true, required: true },
-          { type: "datetime", name: "date", label: "Data Publicării" },
-          { type: "rich-text", name: "body", label: "Corp Articol (Text)", isBody: true },
-          { type: "string", name: "meta_title", label: "SEO Meta Title (În Google)" },
-          { type: "string", name: "meta_description", label: "SEO Meta Description (În Google)", ui: { component: "textarea" } }
+          { type: "datetime", name: "date", label: "Data Publicarii" },
+          { type: "rich-text", name: "body", label: "Corp Articol", isBody: true },
+          { type: "string", name: "meta_title", label: "SEO Meta Title" },
+          { type: "string", name: "meta_description", label: "SEO Meta Description", ui: { component: "textarea" } }
         ],
       },
     ],

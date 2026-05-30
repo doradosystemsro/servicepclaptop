709b6b65d8c39ab510e23b2324d9e7f039b99ba9
doradosystemsro <doradosystems.ro@gmail.com>
2026-05-30 03:57:50 +0300

    Enhance tina/config.mjs with comments and formatting
    Updated configuration with comments for clarity.

--- tina/config.mjs
+++ tina/config.mjs
@@ -2,14 +2,19 @@ import { defineConfig } from "tinacms";
 
 export default defineConfig({
   branch: "main", 
-  clientId: "8640bb63-3016-4565-a65a-62829d5758f7", 
-  token: process.env.TINA_TOKEN, 
+  clientId: "8640bb63-3016-4565-a65a-62829d5758f7", // Client ID-ul tău corect
+  token: process.env.TINA_TOKEN, // Va citi token-ul adăugat de tine în Cloudflare
+  
+  build: {
+    outputFolder: "admin", // Tina va crea interfața în folderul servicepclaptop.pages.dev/admin
+    publicFolder: ".",     // Rădăcina site-ului tău HTML
+  },
   schema: {
     collections: [
       {
         name: "post",
         label: "Articole Blog",
-        path: "blog", 
+        path: "blog", // Salvează direct în folderul tău existent numit blog
         format: "md",
         fields: [
           { type: "string", name: "title", label: "Titlu Articol (H1)", isTitle: true, required: true },

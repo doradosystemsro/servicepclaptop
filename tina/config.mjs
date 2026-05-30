fcf2829a32ecea7c5f243e4471b7732b068ed302
doradosystemsro <doradosystems.ro@gmail.com>
2026-05-30 03:14:10 +0300

    Create config.mjs

---
+++ tina/config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from "tinacms";
+
+export default defineConfig({
+  branch: "main", 
+  clientId: "8640bb63-3016-4565-a65a-62829d5758f7", 
+  token: process.env.TINA_TOKEN, 
+  
+  build: {
+    outputFolder: "admin",
+    publicFolder: ".", 
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Articole Blog",
+        path: "blog", 
+        format: "md",
+        fields: [
+          { type: "string", name: "title", label: "Titlu Articol (H1)", isTitle: true, required: true },
+          { type: "datetime", name: "date", label: "Data Publicării" },
+          { type: "rich-text", name: "body", label: "Corp Articol (Text)", isBody: true },
+          { type: "string", name: "meta_title", label: "SEO Meta Title (În Google)" },
+          { type: "string", name: "meta_description", label: "SEO Meta Description (În Google)", ui: { component: "textarea" } }
+        ],
+      },
+    ],
+  },
+});

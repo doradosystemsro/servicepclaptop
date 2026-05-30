56a705e1fc2ca005ba8803f0b1fb66ba88a4e84d
doradosystemsro <doradosystems.ro@gmail.com>
2026-05-30 08:23:54 +0300

    Update config.js

--- tina/config.js
+++ tina/config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "tinacms";
 export default defineConfig({
   branch: "main",
   clientId: "30229e1e-35c2-4a48-8256-9f2100285071",
-  token: "38a0d1f44a7cd046148f64f395ce6b507a70823a",
+  token: "3fd1f9ec5a37473f9cdafbe3493f8f533e523551",
   build: {
     outputFolder: "admin",
     publicFolder: ".",
@@ -14,6 +14,7 @@ export default defineConfig({
         name: "post",
         label: "Articole Blog",
         path: "blog",
+        format: "html", // ÎI SPUNEM EXPLICIT CĂ AI FIȘIERE .HTML ACOLO!
         fields: [
           {
             type: "string",

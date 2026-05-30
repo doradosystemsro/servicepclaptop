b15b254635bc0b110ba5c103c9ae7fab0b9bdcd9
doradosystemsro <doradosystems.ro@gmail.com>
2026-05-30 06:08:31 +0300

    Create config.js

---
+++ tina/config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from "tinacms";
+
+export default defineConfig({
+  branch: "main",
+  clientId: "8640bb63-3016-4565-a65a-62829d5758f7",
+  token: "c14c4e689dd6159dc6c5af7649cc30a6bc592dac",
+  build: {
+    outputFolder: "admin",
+    publicFolder: ".",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: ".",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Articole Blog",
+        path: "blog",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Titlu",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Continut",
+          },
+        ],
+      },
+    ],
+  },
+});
